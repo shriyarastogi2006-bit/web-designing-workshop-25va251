9ea658421ef8de37bc3290547441d7821871b070
shriyarastogi2006-bit <shriyarastogi2006@gmail.com>
2026-06-30 21:32:09 +0530

    commit

---
+++ lecture1.js
@@ -0,0 +1,17 @@
+const product={
+    name:"parker pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
+const profile={
+    name:"shriya",
+    isfollow:true,
+    post:0,
+    followers:170,
+    following:271,
+
+};
+console.log(profile);
+console.log(typeof profile["name"]);
